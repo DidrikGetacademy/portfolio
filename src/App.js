78222b81c1a2666feeecfb8ccac0d8e1469c26bb
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2025-04-15 18:56:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 import MyProjectsPage from './Pages/MyProjectsPage'
 import IntroPage from "./Pages/IntroPage";
 import './css/App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter >
     <div className="app-container">
       <Routes>
         <Route path="/" element={<IntroPage/>}/>  
         <Route path="/MyProjectsPage" element={<MyProjectsPage/>}/>  
       </Routes>
     </div>
-    </BrowserRouter>
+    </HashRouter >
   );
 }
 
