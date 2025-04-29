c29f73322167a97b3a38ac4bbb9993c44f68c27e
Didrik Nathaniel LLoyd Aasland Skjelbred <141577441+DidrikGetacademy@users.noreply.github.com>
2025-04-29 13:23:04 +0200

    update

--- src/Components/logocomponent.jsx
+++ src/Components/logocomponent.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import tiktok from '../Image/tiktok.png';
 import Instagram from '../Image/insta.png';
 import X from '../Image/xx.png';
+import youtube from '../Image/youtube.png'
 import Styles from "../css/Lcomponent.module.css";
 
 function LComponent() {
   return (
     <div className={Styles.imgcontainer}>
       <div className={Styles.socialicon}>
-        <a href='https://www.tiktok.com/@learnreflectsession?is_from_webapp=1&sender_device=pc'>
+        <a href='https://www.tiktok.com/@motivationlearnreflect'>
           <img src={tiktok} alt="TikTok Logo" className={Styles.socialIcon} />
         </a>
       </div>
       <div className={Styles.socialicon}>
-        <a href='https://www.instagram.com/learnreflects'>
+        <a href='https://www.instagram.com/learnreflects/'>
           <img alt="Instagram" src={Instagram} className={Styles.socialIcon} />
         </a>
       </div>
       <div className={Styles.socialicon}>
-        <a href='https://x.com/learnreflects?s=21'>
+        <a href='https://x.com/LearnReflects'>
           <img src={X} alt="X Logo" className={Styles.socialIcon} />
+        </a>
+      </div>
+      <div className={Styles.socialicon}>
+        <a href='https://www.youtube.com/@MotivationReflectSession'>
+          <img src={youtube} alt="youtube" className={Styles.socialIcon} />
         </a>
       </div>
     </div>
